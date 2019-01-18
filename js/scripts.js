3984509616ae740ebdfd6c79773011c095733269
kohichi horii <kohichi.horii@gmail.com>
2019-01-18 11:22:33 -0800

    Add result and hide result in css

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,11 @@ $(document).ready(function() {
   $("#track").submit(function(event) {
     event.preventDefault();
 
-    var name = $("#name").val();
-    var email = $("#email").val();
-    var comment = $("#comment").val();
-    var interests = $("#interests").val();
+    var nameInput = $("input#name").val();
+    var emailInput = $("input#email").val();
+    var interestInput = $("select#interests").val();
+    var devInput = $("select#dev").val();
+
 
   });
 });
